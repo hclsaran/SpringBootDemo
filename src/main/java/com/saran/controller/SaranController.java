dd11a72a325e60d36f642f2662a45b829e97f813
hclsaran <77113470+hclsaran@users.noreply.github.com>
2022-02-21 06:46:41 -0500

    update the method

--- src/main/java/com/saran/controller/SaranController.java
+++ src/main/java/com/saran/controller/SaranController.java
@@ -5,18 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+ 
 public class SaranController {
 	
 	
-	@GetMapping("/1")
+	@GetMapping("/")
 	public String myBusinessLogic() {
 		return "<h1 style=color:blue>Welcome to my BusinessLogic One</h1>";
 	}
 	
-	@GetMapping("/2")
-	public String myBusinessLogic2() {
-		return "<h1 style=color:red>Welcome to my BusinessLogic22222</h1>";
-	}
-
+	 
 }
